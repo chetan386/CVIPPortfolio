78b14d4854f628c8f8d1c95ce8252c908f7a9db5
chetan <awasthichetan0@gmail.com>
2023-10-04 12:16:53 +0530

    next commit

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
 /* eslint-disable indent */
 /* eslint-disable keyword-spacing */
 import React, { useState, useEffect } from 'react';
@@ -28,7 +30,23 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">Elevating  into <span>Digital Ideas</span> <br /> into  <span> Elegant Code</span></h2>
-
+      <div className="app__testimonial-item app__flex top">
+            <div className="app__testimonial-content mar">
+              <p className="p-text">My name is Chetan Awasthi, a computer science student at  DCRUST Murthal, a passionate programmer who is interested in creating innovative and user-centric web applications. I specialize in the MERN stack, bringing together the power of MongoDB, Express.js, React.js, and Node.js to craft robust and dynamic web solutions.  </p>
+              <div>
+                <a href="https:github.com/chetan386" className="bold-text pad">Github</a>
+                <a href="https://www.linkedin.com/in/chetan-awasthi-749767203" className="bold-text">Linkedin</a>
+              </div>
+            </div>
+          </div>
+          <div className="app__testimonial-brands app__flex">
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+          >
+            {/* <img src={urlFor(brand.imgUrl)} alt={brand.name} /> */}
+          </motion.div>
+      </div>
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
